1c55f67bd5afb895448d60565f2f1d63c8fdc929
kaminofinancebot <dev@kamino.finance>
2024-10-21 16:48:22 +0000

    Update klend-sdk

--- src/lib.ts
+++ src/lib.ts
@@ -2,6 +2,12 @@ export * from './idl_codegen/instructions';
 export * from './idl_codegen/accounts';
 export * from './idl_codegen/programId';
 export * from './idl_codegen/zero_padding';
+
+export * from './idl_codegen_kamino_vault/instructions';
+// only export vault state, do not export Reserve as it's the same one in main klend /idl_codegen/accounts
+export * from './idl_codegen_kamino_vault/accounts/VaultState';
+export * from './idl_codegen_kamino_vault/types';
+
 export * from './classes';
 export * from './utils';
 export * from './leverage';
