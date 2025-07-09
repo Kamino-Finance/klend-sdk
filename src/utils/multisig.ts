847f01fc2326f29def76ca29e8ded44fa1644fcd
kaminofinancebot <dev@kamino.finance>
2025-07-09 17:57:23 +0000

    Update klend-sdk

--- src/utils/multisig.ts
+++ src/utils/multisig.ts
@@ -9,18 +9,25 @@ export async function walletIsSquadsMultisig(wallet: Address) {
   return squadsResponse.isSquad;
 }
 
-// todo: find a way to get the admins number and threshold
 export async function getSquadsMultisigAdminsAndThreshold(wallet: Address): Promise<{
   adminsNumber: number;
   threshold: number;
 }> {
   const response = await fetch(`${SQUADS_API_BASE_URL}/multisig/${wallet}`);
   const data = await response.json();
-  const squadsResponse = data as SquadsMultisigAccountResponse;
-  return {
-    adminsNumber: squadsResponse.keys.length,
-    threshold: squadsResponse.threshold,
-  };
+  try {
+    const squadsResponse = data as SquadsV4MultisigAccountResponse;
+    return {
+      adminsNumber: squadsResponse.account.members.length,
+      threshold: squadsResponse.account.threshold,
+    };
+  } catch (e) {
+    const squadsResponse = data as SquadsV3MultisigAccountResponse;
+    return {
+      adminsNumber: squadsResponse.keys.length,
+      threshold: squadsResponse.threshold,
+    };
+  }
 }
 
 // {"isSquad":true,"version":"v3"}
@@ -35,7 +42,25 @@ export interface WalletType {
   walletThreshold: number;
 }
 
-export type SquadsMultisigAccountResponse = {
+export type SquadsV4MultisigAccountResponse = {
+  account: {
+    bump: number;
+    configAuthority: string;
+    createKey: string;
+    members: number[][];
+    rentCollector: string;
+    staleTransactionIndex: string;
+    threshold: number;
+    timeLock: number;
+    total_signers: number;
+    transactionIndex: string;
+  };
+  address: string;
+  defaultVault: string;
+  metadata: { version: string };
+};
+
+export type SquadsV3MultisigAccountResponse = {
   allow_external_execute: boolean;
   authority_index: number;
   bump: number;
