f808ec6fb216ee419db21e1f9f1fa7db12d1b359
kaminofinancebot <dev@kamino.finance>
2026-03-09 11:48:27 +0000

    Update klend-sdk

--- src/@codegen/unstaking_pool/accounts/index.ts
+++
@@ -1,4 +0,0 @@
-export { PoolState } from "./PoolState"
-export type { PoolStateFields, PoolStateJSON } from "./PoolState"
-export { UnstakeTicket } from "./UnstakeTicket"
-export type { UnstakeTicketFields, UnstakeTicketJSON } from "./UnstakeTicket"
--- src/lib.ts
+++ src/lib.ts
@@ -9,7 +9,6 @@ export { GlobalConfig as KVaultGlobalConfig } from './@codegen/kvault/accounts/G
 export * from './@codegen/kvault/accounts/VaultState';
 export * from './@codegen/kvault/types';
 export { PROGRAM_ID as KVAULTS_PROGRAM_ID } from './@codegen/kvault/programId';
-export { PROGRAM_ID as UNSTAKING_POOL_ID } from './@codegen/unstaking_pool/programId';
 
 export * from './classes';
 export * from './utils';
